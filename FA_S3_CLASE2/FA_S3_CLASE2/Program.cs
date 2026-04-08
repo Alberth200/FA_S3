87f295583c0941cc4103a7debac128c55150cf1b
Alberth200 <N00434497@upn.pe>
2026-04-07 19:19:33 -0500

    Add files via upload

---
+++ FA_S3_CLASE2/FA_S3_CLASE2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA_S3_CLASE2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opción;
+            do {
+                switch (opción)
+                    case 1: break;
+                    case 2: break;
+                    case 3: break;
+                    case 4: break;
+            while (true) }
+            Ej1()
+            
+
+            //Ejercicio 2:
+            //Console.WriteLine("Ingrese base");
+            //float bases = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese altura");
+            //float altura = float.Parse(Console.ReadLine());
+            //Console.WriteLine("El area total es: " + bases*altura/2);
+            //10 20 = 100 resultado
+
+            //Ejercicio 3:
+            //int sis = 20;
+            //int cont = 50;
+            //int adm = 15;
+            //float total = sis + cont + adm;
+            //float porsis = (sis / total) * 100;
+            //float porcont = (cont / total) * 100;
+            //float poradm = (adm / total) * 100;
+            //Console.WriteLine("Ingrese cantidad de estudiantes de Ing.Sistemas: " + porsis);
+            //Console.WriteLine("Ingrese cantidad de estudiantes de Administración: " + poradm);
+            //Console.WriteLine("Ingrese cantidad de estudiantes de Contadores: " + porcont);
+
+            //Ejercicio 4:
+            //Console.WriteLine("Ingrese base");
+            //float bases = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese base");
+            //float altura = float.Parse(Console.ReadLine());
+            //float area = bases * altura;
+            //float perimentro = 2 * (bases + altura);
+            //Console.WriteLine("El área del rectangulo es: " + area);
+            //Console.WriteLine("El perimetro del rectangulo es: " + perimentro);
+
+            //Ejercicio 5 (Volumen del cilindro):
+            //Console.WriteLine("Ingrese radio");
+            //Console.WriteLine("Ingrese altura");
+            //double altura = double.Parse(Console.ReadLine());
+            //double volumen = 3.1415 + (Math.Pow(radio,2)) * altura;
+            //Console.WriteLine("El volumen es: " + volumen );
+        }
+        static void Ej1() 
+        {
+            //Ejercicio 1: (C*9/5)+32
+            //Console.WriteLine("Ingresa grados Celcius");
+            //float celcius = float.Parse(Console.ReadLine());
+            //float fharenheit = (celcius * 9 / 5) + 32;
+            //Console.WriteLine("Grados fharenheit: " + fharenheit); 
+        }
+    }
+}
---
+++ FA_S3_CLASE2/FA_S3_CLASE2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("FA_S3_CLASE2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FA_S3_CLASE2")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("4e033b33-d7bf-4d2b-8d3a-cda8ec72ab86")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
